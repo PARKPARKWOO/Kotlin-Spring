43b1772568f6e25a7ddbd26a6e403b48ab289649
PARKPARKWOO <wy9295@naver.com>
2023-11-07 22:03:43 +0900

    Chore 잘못된 테스트 케이스 수정

--- src/test/kotlin/com/example/kotlin_example/util/SecurityTest.kt
+++ src/test/kotlin/com/example/kotlin_example/util/SecurityTest.kt
@@ -1,22 +1,28 @@
 package com.example.kotlin_example.util
 
+import com.example.kotlin_example.config.security.SecurityConfig
+import io.mockk.every
 import io.mockk.junit5.MockKExtension
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @ExtendWith(MockKExtension::class)
 class SecurityTest {
 
+    private val securityConfig = mockk<SecurityConfig>()
+
     @Test
-    @DisplayName("password 를 암호화 한다")
-    fun passwordEncryptTest() {
-        val encoder = BCryptPasswordEncoder()
-        val encode = encoder.encode("1234")
-        Assertions.assertNotEquals(encode, "1234")
-        val upgradeEncoding = encoder.upgradeEncoding(encode)
-        Assertions.assertEquals(upgradeEncoding, "1234")
+    fun `password 암호화 를 한다`() {
+        every { securityConfig.passwordEncoder() } returns BCryptPasswordEncoder()
+        val passwordEncoder = securityConfig.passwordEncoder()
+        val purePassword = "1234"
+        val encodePassword = passwordEncoder.encode(purePassword)
+
+        Assertions.assertNotEquals(encodePassword, purePassword)
     }
 }
