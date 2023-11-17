996cbabea3be4d1569dd4ba8bfc66119565c3bdb
PARKPARKWOO <wy9295@naver.com>
2023-11-17 23:50:43 +0900

    feat: handle 수정

--- src/main/kotlin/com/example/kotlin_example/config/security/CustomAccessDeniedHandler.kt
+++ src/main/kotlin/com/example/kotlin_example/config/security/CustomAccessDeniedHandler.kt
@@ -6,14 +6,11 @@ import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.web.access.AccessDeniedHandler
 
-// ktlint-disable package-name
-
 class CustomAccessDeniedHandler() : AccessDeniedHandler, Log() {
 
     override fun handle(
         request: HttpServletRequest,
         response: HttpServletResponse,
-
         accessDeniedException: AccessDeniedException?,
     ) {
         log.info("accessDeniedHandler")
